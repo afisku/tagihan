a02fc1e1e85f44d722654ff587bc326fe2e7ceae
SMPIT CODER <smpitalfityan2020@gmail.com>
2023-02-17 17:09:59 +0700

    sampai episode 6

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BerandaOperatorController;
 use App\Http\Controllers\BerandaWaliController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,7 +27,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::prefix('operator')->middleware(['auth', 'auth.operator'])->group(function (){
     // route khusus operator
     Route::get('beranda', [BerandaOperatorController::class, 'index'])->name('operator.beranda');
-
+    Route::resource('user', UserController::class);
 });
 
 Route::prefix('walimurid')->middleware(['auth', 'auth.wali'])->group(function (){
@@ -39,5 +40,6 @@ Route::prefix('admin')->middleware(['auth', 'auth.admin'])->group(function (){
 });
 
 Route::get('logout', function (){
-    Auth::user()->logout();
-});
+    Auth::logout();
+    return redirect('/login');
+})->name('logout');
