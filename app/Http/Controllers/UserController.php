9483b3599d70e5413a2807bfe61b6069176f4120
SMPIT CODER <smpitalfityan2020@gmail.com>
2023-02-20 16:26:01 +0700

    episode 7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,11 +7,7 @@ use \App\Models\User as Model;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function index()
     {
         return view('operator.user_index', [
@@ -21,67 +17,54 @@ class UserController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function create()
     {
-        //
+        $data = [
+            'model' => new \App\Models\User(),
+             'method' => 'POST',
+             'route' => 'user.store',
+             'button' => 'SIMPAN'
+        ];
+        return view('operator.user_form', $data); 
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+   
     public function store(Request $request)
     {
-        //
+        $requestData = $request->Validate([
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'nohp' => 'required|unique:users',
+            'akses' => 'required|in:operator,admin',
+            'password' => 'required'
+        ]);
+        $requestData['password'] = bcrypt($requestData['password']);
+        $requestData['email_verified_at'] = now();
+        Model::create($requestData);
+        flash('data berhasil disimpan');
+        return back();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function destroy($id)
     {
         //
--- resources/views/operator/user_index.blade.php
+++ resources/views/operator/user_index.blade.php
@@ -7,6 +7,7 @@
                 <h5 class="card-header"> Data User </h5>
 
                 <div class="card-body">
+                    <a href="{{ route('user.create') }}" class="btn btn-primary btn-sm">Tambah Data</a>
                     <div class="table-responsive">
                         <table class="table table-striped">
                             <thead>
